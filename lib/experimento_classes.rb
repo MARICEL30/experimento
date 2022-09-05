6ed651cfc471c06a032d7c8b627152a30213673b
Maricel Dragan <mariaceliadragan@gmail.com>
2022-09-05 21:14:14 +0100

    sep changes

--- lib/experimento_classes.rb
+++ lib/experimento_classes.rb
@@ -1,1 +1,38 @@
-#this is an experimento
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  #this gives you an array with the alphabet and the key unique letters
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+    p "cipher.find_index(char):"
+    p cipher.find_index(char)
+    p char
+  end
+  return ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[65 - char.ord]
+  end
+  return plaintext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[65 - char.ord]
+  end
+  return plaintext_chars.join
+end
+
+p encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+p EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL
+# Intended output:
+#
+# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+#
+# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+# => "theswiftfoxjumpedoverthelazydog"
+
